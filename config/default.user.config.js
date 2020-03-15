8faeb88499a1eee01488705505c4a0174d3ad1e6
LOTW <lotw7270@protonmail.com>
2020-03-16 00:17:24 +0100

    UPDATE: Massive Update

--- config/default.user.config.js
+++ config/default.user.config.js
@@ -1,4 +1,34 @@
 const path = require('path')
+const os = require('os');
+
+// THOUGHTS: Is this really needed... (get different network interface address and do something with it...)
+// const getNetworkInterface = () => {
+
+//     let ifaces = os.networkInterfaces();
+
+//     let networkName, networkAddress
+
+//     Object.keys(ifaces).forEach(function (ifname) {
+//         let alias = 0;
+
+//         ifaces[ifname].forEach(function (iface) {
+//             if ('IPv4' !== iface.family || iface.internal !== false) {
+//                 // dont consider internal like 127.0.0.1 and non-ipv4 addresses
+//                 return;
+//             }
+
+//             if (alias >= 1) {
+//                 // interface with multiple ipv4 addresses
+//                 console.log(ifname + ':' + alias, iface.address);
+//             } else {
+//                 // interface with just one ipv4 adress
+//                 console.log(ifname, iface.address);
+//             }
+//             ++alias;
+//         });
+//     });
+// }
+
 
 const user = {
     // New
@@ -33,19 +63,18 @@ const user = {
         },
         relativeTo: path.join(__dirname, '../'),
         primary: {
-            domain: '127.0.0.1',
-            address: '127.0.0.1', // What the domain should point to
-            port: 12388, // Port to access the Qora UI from
+            domain: '0.0.0.0', // used (0.0.0.0) so as to make is available over the network
+            address: '0.0.0.0', // used (0.0.0.0) so as to make is available over the network
+            port: 12388, // Port to access the UI from
             directory: './src/', // Core Qora-lite code.,
             page404: './src/404.html',
             host: '0.0.0.0' // This probably shouldn't be the default...
         },
         plugin: {
-            domain: '127.0.0.1', // '*.domain' is used to host subdomains
-            address: '127.0.0.1',
+            domain: '0.0.0.0', // '*.domain' is used to host subdomains
+            address: '0.0.0.0', // used (0.0.0.0) so as to make is available over the network
             // domain: 'frag.ui'
-            port: 51432, // meh, why not keep it, who knows what kind of stuff people get into
-            // port: 9087, // Disabled. Will now run on the same port as the host
+            port: 12389,
             directory: './plugins', // Where the plugin folders are stored,
             default: 'wallet',
             host: '0.0.0.0' // frag.ui?
