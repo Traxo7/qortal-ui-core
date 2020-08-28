5a6f15b31642690f64fab55b7bb910d9574371f4
lotw7277 <61802101+lotw7277@users.noreply.github.com>
2020-08-28 15:54:51 +0100

    UPDATE: bumped version to v1.2.3-beta.1

--- config/default.user.config.js
+++ config/default.user.config.js
@@ -44,7 +44,7 @@ const user = {
     nodeSettings: {
         pingInterval: 10 * 1000 // (10 secs)
     },
-    version: 'v1.2.3', // TODO: Set this dynamically...
+    version: 'v1.2.3-beta.1', // TODO: Set this dynamically...
     language: 'english', // default...english
     theme: 'light',
     server: {
